de8a80f90afd4c2e6169a3ab774d568a67cca8bb
Toheeb <aladetoheeb5@gmail.com>
2022-10-16 21:11:59 +0100

    Script that display html

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Script that start a flask app with two routes """
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """ prints Hhello """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ Print HBNB """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """ Prints C with a text followed """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    """ Prints python with default text """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    """ Prints the number passed as argument """
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<n>')
+def number_template(n):
+    """ Displays an html page"""
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
