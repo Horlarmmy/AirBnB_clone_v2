3cf4bd762d1b2f6616e697802b0d8b611a08e32a
Alade Toheeb <aladetoheeb5@gmail.com>
2022-10-16 20:28:44 +0100

    Returns a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Script that start a flask app with two routes """
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """ prints Hhello """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ Print HBNB """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """ Prints C with a text followed """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    """ Prints python with default text """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+@app.route('/number/<n>')
+def number(n):
+    """ Prints the number passed as argument """
+    if (type(n) == 'int'):
+        return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
